ffc7add193e79c75a4e067a05ad2fbc2d0004448
Abhi <rohit.yadav@mmfinfotech.in>
2024-12-05 23:42:26 +0530

    Change

--- src/app/auth/verify-otp/page.jsx
+++ src/app/auth/verify-otp/page.jsx
@@ -1,13 +1,14 @@
+"use client";
 
-import VerifyOTP from '@/components/VerifyOtpPage';
+// import VerifyOTP from '@/components/VerifyOtpPage';
 import dynamic from 'next/dynamic';
 
-// const VerifyOtpPage = dynamic(() => import('../../../components/VerifyOtpPage'), {
-//   ssr: false
-// });
+const VerifyOtpPage = dynamic(() => import('@/components/VerifyOtpPage'), {
+  ssr: false
+});
 
 export default function Page() {
   return (
-    <VerifyOTP />
+    <VerifyOtpPage />
   );
 }
